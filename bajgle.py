4da58eb22ca14d56d059b635535a84e99cfbd05c
Karol B <carolinii.contact@gmail.com>
2022-11-22 09:39:21 +0000

    add a greeting and explanation of the rules of the game

--- bajgle.py
+++ bajgle.py
@@ -1,2 +1,9 @@
+import constants
+
+
+def greet(message) -> None:
+    print(message)
+
+
 if __name__ == '__main__':
-    print('Bajgle')
+    print(constants.MESSAGE)
---
+++ constants.py
@@ -0,0 +1,8 @@
+MESSAGE = f"""Bajgle, logiczna gra na dedukcję. Mam na myśli pewien ciąg znaków składający się z trzech cyfr na początku
+i trzech liter na końcu. W ciągu tym znaki i cyfry nie powtarzają się. Spróbuj odgadnąć ten ciąg. Oto wskazówki:
+Gdy mówię:\tOznacza to:
+\tPiko\tJedna cyfra lub znak jest poprawny, ale jest na złej pozycji.
+\tFermi\tJedna cyfra lub znak jest poprawny i znajduje się na odpowiedniej pozycji.
+\tBajgle\tŻadna cyfra ani znak nie jest poprawny.\n
+Na przykład, jeśli tajny ciąg to 248abc, a Ty podasz ciąg 843dbe, wskazówka będzie brzmieć:
+Fermi Piko Fermi."""
