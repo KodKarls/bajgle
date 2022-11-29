cb185b555e8ae1e0fd894681ed5f8500534552e7
Karol B <carolinii.contact@gmail.com>
2022-11-29 06:56:09 +0000

    create the function to get the secret character string

--- bajgle.py
+++ bajgle.py
@@ -1,3 +1,5 @@
+import random
+
 import constants
 
 
@@ -5,5 +7,23 @@ def greet(message) -> None:
     print(message)
 
 
+def get_secret_character_string() -> str:
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    characters = ['a', 'b', 'c', 'd', 'e', 'f']
+
+    random.shuffle(numbers)
+    random.shuffle(characters)
+
+    character_string = ''
+    for i in range(constants.NUM_DIGITS):
+        character_string += str(numbers[i])
+    for i in range(constants.NUM_LETTERS):
+        character_string += characters[i]
+
+    return character_string
+
+
 if __name__ == '__main__':
     print(constants.MESSAGE)
+    secret_character_string = get_secret_character_string()
+    print(secret_character_string)
--- constants.py
+++ constants.py
@@ -1,8 +1,12 @@
-MESSAGE = f"""Bajgle, logiczna gra na dedukcję. Mam na myśli pewien ciąg znaków składający się z trzech cyfr na początku
-i trzech liter na końcu. W ciągu tym znaki i cyfry nie powtarzają się. Spróbuj odgadnąć ten ciąg. Oto wskazówki:
+MESSAGE = f"""Bajgle, logiczna gra na dedukcję. Mam na myśli pewien ciąg znaków składający się z trzech cyfr (od 1 do 9)
+na początku i trzech liter (od a do f) na końcu. W ciągu tym znaki i cyfry nie powtarzają się. Spróbuj odgadnąć ten 
+ciąg. Oto wskazówki:
 Gdy mówię:\tOznacza to:
 \tPiko\tJedna cyfra lub znak jest poprawny, ale jest na złej pozycji.
 \tFermi\tJedna cyfra lub znak jest poprawny i znajduje się na odpowiedniej pozycji.
 \tBajgle\tŻadna cyfra ani znak nie jest poprawny.\n
 Na przykład, jeśli tajny ciąg to 248abc, a Ty podasz ciąg 843dbe, wskazówka będzie brzmieć:
 Fermi Piko Fermi."""
+
+NUM_DIGITS = 3
+NUM_LETTERS = 3
